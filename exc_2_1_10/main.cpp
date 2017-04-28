a5dea72391a0eb24331871029306beefaa1984bf
albkharisov <albkharisov@gmail.com>
2017-04-29 02:13:02 +0300

    2_1_10: almost solved

--- exc_2_1_10/main.cpp
+++ exc_2_1_10/main.cpp
@@ -5,153 +5,130 @@
 
 using namespace std;
 
-int main()
+void differentiate(map<int,int> &m);
+void print_manyChlen(map<int,int> &m);
+void print_manyChlenNorm(map<int,int> &m);
+void fill_map_from_string(map<int,int> &m, string &input);
+
+
+void fill_map_from_string(map<int,int> &m, string &input)
 {
-	/**
-     * 1) x
-	 * 2) 1*x
-     * 3) 1*x^2
-     * 4) 1
-	 */
-
-	string input;
-	string input2 = "x^1+2*x+3+4*x^5";
-	cout << input2 << endl;
-//	regex integer("(\\+{0,2}|-{0,2})[[:d:]]+");
-//	regex myreg("(+|-)?    [[:d:]]*    x?    (\^[[:d]])?    ");
-//	regex myreg("(+|-)?    [[:d:]]*  \*?   (x  (\^[[:d]]+)*  )?    ");
-//	regex myreg("(\\+|-)?  [[:d:]]* \\*?  (x(\\^[[:d]]+)*)?    ");
-//	regex myreg("(\\+|-)?[[:d:]]*\\*?(x(\\^[[:d:]]+)*)?");
-//	regex myreg("(\\+|-)?\\d*\\*?(x(\\^\\d+)*)?");
-//	regex myreg("(\\+|-)?     \\d*\\*?    (x    (\\^\\d+)*)?");
-//	regex myreg("(\\+|-)?     (\\d+   |    (\\d+\\*)?    (x)  (\\^\\d+)?)");
-//	regex myreg("((\\+|-)?    (\\d+   |   ( (\\d+\\*)?   x    (\\^\\d+)?))     )");
-//	regex myreg("((\\+|-)?(\\d+|((\\d+\\*)?x(\\^\\d+)?)))");                                    // OK
-//	regex myreg("( ((\\+|-)?\\d+)    |     ((\\+|-)?   (\\d+\\*)?x(\\^\\d+)?)  )");
-//	regex myreg("(((\\+|-)?\\d+)|((\\+|-)?(\\d+\\*)?x(\\^\\d+)?))");
-	/*
-	regex myreg("((\\+|-)?\\d{1,})"
-                "|"
-                "((\\+|-)?"
-                "(\\d+\\*)?"
-                "x"
-                "(\\^\\d+)?)");
-	regex myreg("((\\+|-)?\\d+)"
-                "|"
-                "((\\+|-)?"         // +
-                "(\\d*)?"           // 12
-                "\\*"               // *
-                "x"                 // x
-                "\\^"               // ^
-                "(\\d*))");         // 10           // GREAT
-	regex myreg(
-                "((\\+|-)?"         // +
-                "(\\d*)?"           // 12
-                "\\*?"               // *
-                "x"                 // x
-                "\\^"               // ^
-                "(\\d*))"
-                "|"
-                "((\\+|-)?\\d+)");         // 10
-                */
-//	regex myreg("((\\+|-)?(\\d*)?\\*?x\\^(\\d*))|((\\+|-)?\\d+)");
-
     /**          (              1               ) (      5     ) */
     /**           (  2  ) (  3 )          (  4 )    (  6 ) (  7 )*/
 	regex myreg("((\\+|-)?(\\d*)?\\*?x\\^?(\\d*))|((\\+|-)?(\\d+))");
-//	regex myreg("((\\+|-)?  (\\d*)?  \\*?    x   \\^?  (\\d*))   |    ((\\+|-)?\\d+)");
 	cmatch cm;
 
-	map<int, int> m;
-	//As long as the input is correct ask for another number
-	while(true)
-	{
-		cout<<"Give me an integer!"<<endl;
-		cin>>input;
-		if(!cin) break;
-		//Exit when the user inputs q
-		if(input=="q")
-			break;
-
-		cout << "==============================" << endl;
-#if 1
-        for (int i = 0; regex_search(input2.c_str(), cm, myreg); ++i, input2 = cm.suffix().str())
+    for (int i = 0; regex_search(input.c_str(), cm, myreg); ++i, input = cm.suffix().str())
+    {
+        int st = 99;
+        int vl = 99;
+        if (cm[1] != "")
         {
-            cout << i << ":" << cm[0];
-            for (unsigned i = 1; i < cm.size(); ++i )
-                cout << "  | " << cm[i];
-            cout << endl;
-            cout << endl;
-            cout << endl;
-            int st = 99;
-            int vl = 99;
-            if (cm[1] != "")
-            {
-                st = (cm[4] == "") ? 1 : atoi(cm[4].str().c_str());
-                vl = (cm[3] == "") ? 1 : atoi(cm[3].str().c_str());
+            st = (cm[4] == "") ? 1 : atoi(cm[4].str().c_str());
+            vl = (cm[3] == "") ? 1 : atoi(cm[3].str().c_str());
 
-                if (cm[2] == "-")
-                    vl = -vl;
+            if (cm[2] == "-")
+                vl = -vl;
 
-                m[st] += vl;
-            }
-            else{
-                st = 0;
-                vl = atoi(cm[7].str().c_str());
-                if (cm[6] == "-")
-                    vl = -vl;
-
-                m[st] += vl;
-            }
-
-            cout << "st=" << st << " val=" << vl << endl;
-            cout << endl;
-            cout << endl;
-            cout << endl;
+            m[st] += vl;
         }
+        else{
+            st = 0;
+            vl = atoi(cm[7].str().c_str());
+            if (cm[6] == "-")
+                vl = -vl;
 
-/*
-        for(map<string, pair<string,string> >::const_iterator it = m.begin();
-            it != m.end(); ++it)
-        {
-            std::cout << it->first << " " << it->second.first << " " << it->second.second << "\n";
+            m[st] += vl;
         }
-*/
-        for(auto it = m.begin(); it != m.end(); ++it)
-        {
-            std::cout << it->first << " " << it->second << endl;
-        }
+    }
 
-
-		break;
-#elif 1
-        string s = input;
-        regex e = myreg;
-        while (regex_search (s.c_str(),cm,e))
-        {
-            for (auto x:cm) cout << x << " ";
-            cout << endl;
-            s = cm.suffix().str();
-        }
-#elif 1
-//        string::const_iterator searchStart( input2.cbegin() );
-        string::iterator searchStart( input2.begin() );
-		if( regex_search(searchStart, input2.end(), cm, myreg) )
-        {
-            cout << cm[0] << endl;
-            cout << "-------------------------------" << endl;
-            searchStart += cm.position() + cm.length();
-        }
-#else
-        copy( sregex_token_iterator(input.begin(), input.end(), myreg, -1),
-            sregex_token_iterator(),
-            ostream_iterator<string>(cout, "\n"));
-#endif
-
-		cout << "==============================" << endl;
-	}
 }
-/*
-*/
+
+void print_manyChlenNorm(map<int,int> &m)
+{
+    bool notfirst = false;
+    for(auto it = m.rbegin(); it != m.rend(); ++it)
+    {
+//        cout << it->second << "*x^" << it->first;
+//        continue;
+        int st = it->first;
+        int vl = it->second;
+        if (!notfirst)
+        {
+            notfirst = true;
+        }
+        else{
+            if (vl > 0)
+                cout << "+";
+            else if ((vl == -1) && (st != 0))
+                cout << "-";
+        }
+
+        if (vl == 0)
+            continue;
+        else if (st == 0)
+            cout << vl;
+        else if (((vl == 1) || (vl == -1)) && (st == 1))
+            cout << "x";
+        else if (st == 1)
+            cout << vl << "*x";
+        else if ((vl == 1) || (vl == -1))
+            cout << "x^" << st;
+        else
+            cout << vl << "*x^" << st;
+    }
+    cout << endl;
+}
+
+void differentiate(map<int,int> &m)
+{
+    for (auto it = m.begin(); it != m.end(); ++it)
+    {
+        int st = it->first;
+        int vl = it->second;
+        m.erase(st);
+        if (st == 0)
+            continue;
+        vl *= st;
+        --st;
+        m[st] += vl;
+    }
+}
+
+int main(void)
+{
+	string input;
+	map<int, int> m;
+
+	while (1)
+    {
+        cin >> input;
+
+        fill_map_from_string(m, input);
+
+        print_manyChlenNorm(m);
+        differentiate(m);
+        print_manyChlenNorm(m);
+
+        cout << endl << endl;
+        m.clear();
+    }
+
+}
+
+
+
+
+void print_manyChlen(map<int,int> &m)
+{
+    for(auto it = m.begin(); it != m.end(); ++it)
+    {
+        std::cout << it->first << " " << it->second << endl;
+    }
+}
+
+
+
+
 
 
